354b815208858eb43f89509b83c7191bed8ca76c
ahmedibrahim240 <Ahmedibrahim1310@ilcoud.com>
2022-01-07 16:01:08 +0200

    remove import

--- lib/screens/account_screen/account_widget/order_history_screen/order_widget/order_history_body.dart
+++ lib/screens/account_screen/account_widget/order_history_screen/order_widget/order_history_body.dart
@@ -1,6 +1,5 @@
 import 'package:ecommerce_app/core/constans/constans.dart';
-import 'package:ecommerce_app/core/cutom_widget/custom_text.dart';
-import 'package:ecommerce_app/models/order_history_models.dart';
+
 import 'package:ecommerce_app/screens/account_screen/account_widget/account_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
